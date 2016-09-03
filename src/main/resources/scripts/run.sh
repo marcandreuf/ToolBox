63e2969f2b71234b52fc69d45427e5195006b939
Marc Andreu <marcandreuf@gmail.com>
2016-09-03 13:06:11 +0200

    Prepare systemd scripts.

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -3,8 +3,7 @@
 # Arguments:
 # 1 = origin folder to listen for create events
 # 2 = destination folder where to store files organised by subfolders year/month/day
-# 3 = log file name
-
-nohup java -jar toolsbox.jar $1 $2 > $3.log 2>&1 &
+# 3 = location where to move the files that failed in the process
+# 4 = log file name.
 
 nohup java -jar ToolBox.jar /home/marc/archbot/origin/ /home/marc/archbot/dest /home/marc/archbot/failed/ > archbot.log 2>&1 &
